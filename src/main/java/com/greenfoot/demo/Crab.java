1828b9920c6d14cc647a934adee96db3ba0b84ba
thiago <thiagoprocaci@gmail.com>
2019-08-07 21:33:29 -0300

    little crab

---
+++ src/main/java/com/greenfoot/demo/Crab.java
@@ -0,0 +1,16 @@
+package com.greenfoot.demo;
+
+import greenfoot.*;
+
+/**
+ * This class defines a crab. Crabs live on the beach.
+ */
+public class Crab extends Actor
+{
+    public void act()
+    {
+        move(2);
+    }
+}
+
+
---
+++ src/main/java/com/greenfoot/demo/CrabWorld.java
@@ -0,0 +1,16 @@
+package com.greenfoot.demo;
+
+import greenfoot.*;  // (Actor, World, Greenfoot, GreenfootImage)
+
+public class CrabWorld extends World
+{
+    /**
+     * Create the crab world (the beach). Our world has a size 
+     * of 560x560 cells, where every cell is just 1 pixel.
+     */
+    public CrabWorld() 
+    {
+        super(560, 560, 1);
+        this.addObject(new Crab(), 100, 100);
+    }
+}
