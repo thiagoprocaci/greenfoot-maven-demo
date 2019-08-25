66050a814712ac70219148678ceabe71f5c07c34
thiago <thiagoprocaci@gmail.com>
2019-08-25 16:13:42 -0300

    little-crab-5

--- src/main/java/com/greenfoot/demo/CrabWorld.java
+++ src/main/java/com/greenfoot/demo/CrabWorld.java
@@ -1,7 +1,11 @@
 package com.greenfoot.demo;
 
-import greenfoot.*;  // (Actor, World, Greenfoot, GreenfootImage)
+import greenfoot.*;  // imports Actor, World, Greenfoot, GreenfootImage
 
+/**
+ * The CrabWorld is the place where crabs and other creatures live. 
+ * It creates the initial population.
+ */
 public class CrabWorld extends World
 {
     /**
@@ -11,6 +15,42 @@ public class CrabWorld extends World
     public CrabWorld() 
     {
         super(560, 560, 1);
-        this.addObject(new Crab(), 100, 100);
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program. That is: create the initial
+     * objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Crab crab = new Crab();
+        addObject(crab, 231, 203);
+        Worm worm = new Worm();
+        addObject(worm, 445, 137);
+        Worm worm2 = new Worm();
+        addObject(worm2, 454, 369);
+        Worm worm3 = new Worm();
+        addObject(worm3, 368, 466);
+        Worm worm4 = new Worm();
+        addObject(worm4, 129, 488);
+        Worm worm5 = new Worm();
+        addObject(worm5, 254, 388);
+        Worm worm6 = new Worm();
+        addObject(worm6, 106, 334);
+        Worm worm7 = new Worm();
+        addObject(worm7, 338, 112);
+        Worm worm8 = new Worm();
+        addObject(worm8, 150, 94);
+        Worm worm9 = new Worm();
+        addObject(worm9, 373, 240);
+        Worm worm10 = new Worm();
+        addObject(worm10, 509, 55);
+        Lobster lobster = new Lobster();
+        addObject(lobster, 334, 65);
+        Lobster lobster2 = new Lobster();
+        addObject(lobster2, 481, 481);
+        Lobster lobster3 = new Lobster();
+        addObject(lobster3, 79, 270);
     }
 }
---
+++ src/main/java/com/greenfoot/demo/Worm.java
@@ -0,0 +1,11 @@
+package com.greenfoot.demo;
+
+import greenfoot.*;
+
+/**
+ * Worm. A sand worm. Very yummy. Especially crabs really like it.
+ */
+public class Worm extends Actor
+{
+    
+}
